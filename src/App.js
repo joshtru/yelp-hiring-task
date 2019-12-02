f3e1f861058c25e39cbcb6284ba25d1c757932c1
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-02 11:09:27 -0600

    Properly display map from google map api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+// IMPORTING STYLES
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = process.env.REACT_APP_API_KEY_SECRET;
+
+class App extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: 35.468,
+      lng: -97.521
+    },
+    zoom: 11
+  };
+  render() {
+    return (
+      <div className="App" style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        ></GoogleMapReact>
+      </div>
+    );
+  }
 }
 
 export default App;
