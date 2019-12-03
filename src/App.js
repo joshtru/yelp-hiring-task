d6329ea7f7ab208a768fc3b9c62a3fe1364ee620
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-03 15:05:09 -0600

    Move search into a its component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import Script from "react-load-script";
 // IMPORTING STYLES
 import styles from "./App.module.css";
 // IMPORTING COMPONENTS
 import SideBar from "./components/sideBar/sideBar.component";
 import LocationButton from "./components/locationButton/locationButton.component";
+import Search from "./components/search/search.component";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
-      gettingLocation: false,
-      searchValue: "",
-      businesses: [],
-      region: {},
       city: "",
       query: "",
       hasMounted: false
@@ -28,44 +23,6 @@ class App extends React.Component {
     },
     zoom: 11
   };
-
-  // HANDLE SCRIPT
-  handleScriptLoad = () => {
-    // Declare Options For Autocomplete
-    const options = {
-      types: ["(cities)"]
-    }; // To disable any eslint 'google not defined' errors
-
-    // Initialize Google Autocomplete
-    /*global google*/ this.autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById("autocomplete")
-      // options
-    );
-    console.log(this.autocomplete);
-
-    // Avoid paying for data that you don't need by restricting the set of
-    // place fields that are returned to just the address components and formatted
-    // address.
-    this.autocomplete.setFields(["address_components", "formatted_address"]);
-
-    // Fire Event when a suggested name is selected
-    this.autocomplete.addListener("place_changed", this.handlePlaceSelect);
-  };
-  // HANDLE PLACE SELECT
-
-  handlePlaceSelect = () => {
-    // Extract City From Address Object
-    const addressObject = this.autocomplete.getPlace();
-    const address = addressObject.address_components;
-    // Check if address is valid
-    if (address) {
-      // Set State
-      this.setState({
-        city: address[0].long_name,
-        query: addressObject.formatted_address
-      });
-    }
-  };
   render() {
     const { query, city } = this.state;
     console.log(query, "City", city);
@@ -76,22 +33,7 @@ class App extends React.Component {
         {/* LOCATION BUTTON */}
         <LocationButton />
         {/* SEARCH SECTION */}
-        <div className={styles.search__container}>
-          <input
-            className={styles.search__input}
-            id="autocomplete"
-            type="search"
-            placeholder="Search City"
-          />
-          {/* {this.hasMounted ? ( */}
-          {/* <Script
-            type="text/javascript"
-            url={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_KEY}&libraries=places`}
-            defer
-            onLoad={this.handleScriptLoad}
-          /> */}
-          {/* ) : null} */}
-        </div>
+        <Search />
         <div className={styles.map__container}>
           <GoogleMapReact
             // bootstrapURLKeys={{ key: GOOGLE_KEY }}
---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Script from "react-load-script";
+
+// IMPORTING STYLES
+import styles from "./search.module.css";
+const GOOGLE_KEY = process.env.REACT_APP_SECRET_MAP_GOOGLE;
+const Search = () => {
+  // HANDLE SCRIPT
+  const handleScriptLoad = () => {
+    // Declare Options For Autocomplete
+    let autocomplete = null;
+    const options = {
+      types: ["(cities)"]
+    }; // To disable any eslint 'google not defined' errors
+
+    // Initialize Google Autocomplete
+    /*global google*/ autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete")
+      // options
+    );
+    console.log(autocomplete);
+
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components and formatted
+    // address.
+    autocomplete.setFields(["address_components", "formatted_address"]);
+
+    // Fire Event when a suggested name is selected
+    autocomplete.addListener("place_changed", handlePlaceSelect);
+  };
+  // HANDLE PLACE SELECT
+
+  const handlePlaceSelect = autocomplete => {
+    // Extract City From Address Object
+    const addressObject = autocomplete.getPlace();
+    const address = addressObject.address_components;
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState({
+        city: address[0].long_name,
+        query: addressObject.formatted_address
+      });
+    }
+  };
+  return (
+    <div className={styles.search__container}>
+      <input
+        className={styles.search__input}
+        id="autocomplete"
+        type="search"
+        placeholder="Search City"
+      />
+      <Script
+        type="text/javascript"
+        url={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_KEY}&libraries=places`}
+        defer
+        onLoad={handleScriptLoad}
+      />
+    </div>
+  );
+};
+
+export default Search;
