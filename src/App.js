8843b495c491ef9a50f096ad9fb8a5a2253cd3c8
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-02 18:33:18 -0600

    Using api for dropdown list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
-import GoogleMapReact, { Google } from "google-map-react";
+import GoogleMapReact from "google-map-react";
 import axios from "axios";
 // IMPORTING STYLES
 import styles from "./App.module.css";
 // IMPORTING COMPONENTS
 import SideBar from "./components/sideBar/sideBar.component";
-import Search from "./components/search/search.component";
 
-const API_KEY = process.env.REACT_APP_SECRET_MAP_GOOGLE;
+const GOOGLE_KEY = process.env.REACT_APP_SECRET_MAP_GOOGLE;
+const YELP_KEY = process.env.REACT_APP_SECRET_YELP_KEY;
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      userLat: null,
-      userLong: null,
+      userLat: 36.280388,
+      userLong: -95.845398,
       error: null,
-      gettingLocation: false
+      gettingLocation: false,
+      searchValue: "",
+      suggestions: {
+        businesses: [],
+        categories: [],
+        terms: []
+      }
     };
   }
   static defaultProps = {
@@ -62,8 +68,58 @@ class App extends React.Component {
     }
     this.setState({ error: receivedError, gettingLocation: false });
   };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+  handleChange = event => {
+    const { value } = event.target;
+    const { userLat, userLong } = this.state;
+    this.setState({ searchValue: value });
+    axios
+      .get(
+        `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/autocomplete`,
+        {
+          params: {
+            text: value
+            // latitude: userLat,
+            // longitude: userLong
+          },
+          headers: {
+            Authorization: `Bearer ${YELP_KEY}`
+          }
+        }
+      )
+      .then(response => {
+        this.setState({
+          suggestions: {
+            businesses: response.data.businesses,
+            categories: response.data.categories,
+            terms: response.data.terms
+          }
+        });
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(`ERROR: ${error}`);
+        this.setState({
+          suggestions: {
+            businesses: [],
+            categories: [],
+            terms: []
+          }
+        });
+      });
+  };
   render() {
-    const { gettingLocation, error } = this.state;
+    const {
+      gettingLocation,
+      error,
+      searchValue,
+      suggestions: { businesses, terms, categories }
+    } = this.state;
+    console.log(terms);
+    console.log(searchValue);
     return (
       <div className={styles.App}>
         {/* SIDE BAR */}
@@ -83,21 +139,45 @@ class App extends React.Component {
         </button>
         {/* SEARCH SECTION */}
         <div className={styles.search__container}>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input
               className={styles.search__input}
               type="search"
+              name="search"
+              onChange={this.handleChange}
               placeholder="Search Restaurants"
+              autoComplete="off"
             />
           </form>
           <div className={styles.autocomplete__box}>
             <ul>
-              <li>Hello</li>
-              <li>Hello</li>
-              <li>Hello</li>
-              <li>Hello</li>
-              <li>Hello</li>
-              <li>world</li>
+              {/* BUSINESSES */}
+              {businesses.map(value => (
+                <li
+                  key={value}
+                  onClick={() => this.setState({ searchValue: value })}
+                >
+                  {value}
+                </li>
+              ))}
+              {/* CATEGORIES */}
+              {categories.map(value => (
+                <li
+                  key={value.alias}
+                  onClick={() => this.setState({ searchValue: value.title })}
+                >
+                  {value.title}
+                </li>
+              ))}
+              {/* TERMS */}
+              {terms.map(value => (
+                <li
+                  key={value.text}
+                  onClick={() => this.setState({ searchValue: value.text })}
+                >
+                  {value.text}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
