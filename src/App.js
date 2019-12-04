2e408014cf70306897710f17f6530675d808f398
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-04 10:20:47 -0600

    Cleaned and made ready App component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import React from "react";
-import GoogleMapReact from "google-map-react";
 // IMPORTING STYLES
 import styles from "./App.module.css";
 // IMPORTING COMPONENTS
 import SideBar from "./components/sideBar/sideBar.component";
 import LocationButton from "./components/locationButton/locationButton.component";
 import Search from "./components/search/search.component";
+import GoogleMapContainer from "./components/googleMapContainer/googleMapContainer.component";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: "",
-      query: "",
-      hasMounted: false
-    };
-  }
-  static defaultProps = {
-    center: {
-      lat: 35.468,
-      lng: -97.521
-    },
-    zoom: 11
-  };
-  render() {
-    const { query, city } = this.state;
-    console.log(query, "City", city);
-    return (
-      <div className={styles.App}>
-        {/* SIDE BAR */}
-        <SideBar />
-        {/* LOCATION BUTTON */}
-        <LocationButton />
-        {/* SEARCH SECTION */}
-        <Search />
-        <div className={styles.map__container}>
-          <GoogleMapReact
-            // bootstrapURLKeys={{ key: GOOGLE_KEY }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-          ></GoogleMapReact>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className={styles.App}>
+    {/* SIDE BAR to display list of found business*/}
+    <SideBar />
+    {/* LOCATION BUTTON to get user's location */}
+    <LocationButton />
+    {/* SEARCH SECTION to search for location */}
+    <Search />
+    {/* GOOGLE MAP CONTAINER to display google map and map markers */}
+    <GoogleMapContainer />
+  </div>
+);
 
 export default App;
