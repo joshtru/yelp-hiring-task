05362f96f098e1261b8c0d3655db32892bccd9de
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-04 14:33:09 -0600

    Added more test

--- src/components/ratingBar/ratingBar.test.js
+++ src/components/ratingBar/ratingBar.test.js
@@ -2,10 +2,27 @@ import { shallow } from "enzyme";
 import React from "react";
 import RatingBar from "./ratingBar.component";
 describe("Rating Star Bar Component", () => {
-  test("Expecting component to return Nothing when passed no props", () => {
+  test("Expecting component to render", () => {
     expect(shallow(<RatingBar />)).toEqual({});
   });
-  test("Expecting component to return no value with props passed", () => {
-    expect(shallow(<RatingBar rating={4.5} />)).toEqual({});
+  test("Expecting to undefined", () => {
+    const wrapper = shallow(<RatingBar rating={-1} />);
+    expect(wrapper.find("img").prop("src")).toEqual(undefined);
+  });
+  test("Expecting to return image file for small 0", () => {
+    const wrapper = shallow(<RatingBar rating={0} />);
+    expect(wrapper.find("img").prop("src")).toEqual("small_0.png");
+  });
+  test("Expecting to return image file for small 4", () => {
+    const wrapper = shallow(<RatingBar rating={4} />);
+    expect(wrapper.find("img").prop("src")).toEqual("small_4.png");
+  });
+  test("Expecting to return image file for small 5", () => {
+    const wrapper = shallow(<RatingBar rating={5} />);
+    expect(wrapper.find("img").prop("src")).toEqual("small_5.png");
+  });
+  test("Expecting to return undefined", () => {
+    const wrapper = shallow(<RatingBar rating={5.5} />);
+    expect(wrapper.find("img").prop("src")).toEqual(undefined);
   });
 });
