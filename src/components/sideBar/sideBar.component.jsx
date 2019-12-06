5f24b3a7ca39903f0478c5676201fdde8c476633
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-06 15:49:16 -0600

    Response if no restaurant found

--- src/components/sideBar/sideBar.component.jsx
+++ src/components/sideBar/sideBar.component.jsx
@@ -12,19 +12,26 @@ import styles from "./sideBar.module.css";
 const SideBar = ({ listOfRestaurants, showMap }) => {
   return !showMap & (listOfRestaurants !== null) ? (
     <div className={styles.sideBar}>
-      {listOfRestaurants.businesses.map(list => (
-        <Section
-          key={list.id}
-          name={list.name}
-          rating={list.rating}
-          review_count={list.review_count}
-          categories={list.categories}
-          isClosed={list.is_closed}
-          location={list.location.address1}
-          imageUrl={list.image_url}
-          linkToYelp={list.url}
-        />
-      ))}
+      {listOfRestaurants.businesses.length === 0 ? (
+        <div>
+          <h3>No Restaurants Found</h3>
+          <p>Check another location</p>
+        </div>
+      ) : (
+        listOfRestaurants.businesses.map(list => (
+          <Section
+            key={list.id}
+            name={list.name}
+            rating={list.rating}
+            review_count={list.review_count}
+            categories={list.categories}
+            isClosed={list.is_closed}
+            location={list.location.address1}
+            imageUrl={list.image_url}
+            linkToYelp={list.url}
+          />
+        ))
+      )}
     </div>
   ) : null;
 };
